d35f3fdfdfac904ac170637dc22d27c50c5ba673
zhourunkai <944268618@qq.com>
2021-04-21 11:08:19 +0800

    feat(js): es6

---
+++ js/es6/09.generator.js
@@ -0,0 +1,10 @@
+function* helloWorldGenerator() {
+  yield 'hello';
+  yield 'world';
+  return 'ending';
+}
+var hw = helloWorldGenerator();
+
+console.log(hw.next()); // { value: 'hello', done: false }
+console.log(hw.next()); // { value: 'world', done: false }
+console.log(hw.next()); // { value: 'ending', done: true }
