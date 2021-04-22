4c30d37c89629aa3000261d4e54ef6b51112b79c
zhourunkai <944268618@qq.com>
2021-04-22 10:02:11 +0800

    feat(webpack): react

---
+++ webpack-demo/react/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>HELLO REACT</h1>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
