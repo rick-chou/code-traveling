96e92e78e590029ef5a562e9b3b54d4eccc41ea9
zhourunkai <944268618@qq.com>
2021-04-22 21:07:46 +0800

    feat(webpack): vue

---
+++ webpack-demo/vue/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Hello></Hello>
+</template>
+
+
+<script>
+import Hello from "./src/components/Hello";
+
+export default {
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ webpack-demo/vue/config/webpack.config.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, '../public/index.html'),
+});
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '../main.js'),
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'js/[name].[hash].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        use: ['vue-loader'],
+      },
+    ],
+  },
+
+  plugins: [htmlWebpackPlugin, new VueLoaderPlugin()],
+
+  resolve: {
+    extensions: ['.vue'],
+  },
+};
