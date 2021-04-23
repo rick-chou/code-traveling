77c8ad0127b32854b9ade1bdc151f0ceb4c899e2
zhourunkai <944268618@qq.com>
2021-04-23 10:01:00 +0800

    feat(js): es6

--- js/es6/08.Promise.js
+++ js/es6/08.Promise.js
@@ -1,11 +1,21 @@
-const flag = true;
+// const flag = true;
 
-const myPromise = new Promise((resolve, reject) => {
-  if (flag) {
-    resolve('hello promise');
-  } else {
-    resolve('bye promise');
-  }
-});
+// const myPromise = new Promise((resolve, reject) => {
+//   if (flag) {
+//     resolve('hello promise');
+//   } else {
+//     resolve('bye promise');
+//   }
+// });
 
-myPromise.then((res) => console.log(res)).catch((res) => console.log(res));
+// myPromise.then((res) => console.log(res)).catch((res) => console.log(res));
+
+const promise1 = Promise.reject(new Error('fail'))
+  .then((res) => console.log(res))
+  .catch((rej) => console.log(rej));
+const promise2 = Promise.resolve('yeah');
+
+// Promise.all([promise1, promise2]).then((res) => console.log(res));
+// Promise.any([promise1, promise2]).then((res) => console.log(res));
+// Promise.allSettled([promise1, promise2]).then((res) => console.log(res));
+// Promise.race([promise1, promise2]).then((res) => console.log(res));
