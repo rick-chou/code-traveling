f7d2cf61b8c8fa3ed77b67e2e0ef3e348e48295f
zhourunkai <944268618@qq.com>
2021-04-27 09:11:56 +0800

    feat(react): vite ts hook

--- react-ts-vite/vite.config.ts
+++ react-ts-vite/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+const resolve = require('resolve');
+const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+      '@hooks': '@/hooks',
+      '@router': '@/router',
+      '@store': '@/store',
+      '@containers': '@/containers',
+      '@components': '@/components',
+    },
+  },
+});
