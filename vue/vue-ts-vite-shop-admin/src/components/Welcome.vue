5ecbce7ed60178a1149ddc7ea20b4efe61df4cc2
zhourunkai <944268618@qq.com>
2021-04-28 11:43:27 +0800

    feat(vue): init vue-demo

---
+++ vue/vue-ts-vite-shop-admin/src/components/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- <img src="../assets/bg.jpg" /> -->
+  </div>
+</template>
+
+<style lang="scss">
+img {
+  width: 100%;
+}
+</style>
---
+++ vue/vue-ts-vite-shop-admin/src/components/order/Order.vue
@@ -0,0 +1,183 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+      <!-- 订单列表数据 -->
+      <el-table :data="orderlist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template v-slot="scope">
+            <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send">
+          <template v-slot="scope">
+            <template>
+              {{ scope.row.is_send }}
+            </template>
+          </template>
+        </el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template v-slot="scope">
+            {{ scope.row.create_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="收货地址" prop="consignee_addr" width="350">
+          <template v-slot="scope">
+            {{ scope.row.consignee_addr }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="70">
+          <template>
+            <!-- 修改 -->
+            <el-button size="mini" type="primary" icon="el-icon-edit" @click="showBox"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
+
+    <!-- 修改地址的对话框 -->
+    <el-dialog title="修改地址" :visible.sync="addressVisible" width="50%" @close="addressDialogClosed">
+      <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader :options="cityData" v-model="addressForm.address1"></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addressVisible = false">取 消</el-button>
+        <el-button type="primary" @click="changeVisible('addressFormRef')">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 展示物流进度的对话框 -->
+    <el-dialog title="物流进度" :visible.sync="progressVisible" width="50%">
+      <!-- 时间线 -->
+      <el-timeline>
+        <el-timeline-item v-for="(activity, index) in progressInfo" :key="index" :timestamp="activity.time">
+          {{ activity.context }}
+        </el-timeline-item>
+      </el-timeline>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import cityData from './citydata.js'
+
+export default {
+  data() {
+    var validateAddress1 = (rule, value, callback) => {
+      if (this.addressForm.address1.length === 0) {
+        callback(new Error('请选择省市区县'))
+      }
+    }
+    var validateAddress2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请填写详细地址'))
+      }
+    }
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 5,
+      },
+      total: 0,
+      orderlist: [],
+      // 修改地址modal是否可见
+      addressVisible: false,
+      // 修改收货地址
+      addressForm: {
+        address1: [],
+        address2: '',
+      },
+      addressFormRules: {
+        address1: [{ tirgger: 'blur', validator: validateAddress1 }],
+        address2: [{ trigger: 'blur', validator: validateAddress2 }],
+      },
+      cityData,
+      progressVisible: false,
+      progressInfo: [],
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    changeVisible(formName) {
+      if (this.addressForm.address1.length !== 0 && this.addressForm.address2 !== '') {
+        this.addressVisible = false
+      } else {
+        this.$refs[formName].validate((valid) => {
+          return false
+        })
+      }
+    },
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败！')
+      }
+      this.$message.success('获取商品列表成功！')
+      this.total = res.data.total
+      this.orderlist = res.data.goods
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    },
+    // 展示修改地址的对话框
+    showBox() {
+      this.addressVisible = true
+    },
+    addressDialogClosed() {
+      this.$refs.addressFormRef.resetFields()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.el-cascader {
+  width: 100%;
+}
+</style>
