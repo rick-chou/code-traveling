c5061838f040bf7632fb9ec7d6b697db2b1bf87b
zhourunkai <944268618@qq.com>
2021-05-17 17:42:34 +0800

    feat(react): ssr

--- react/ssr-demo/app.ts
+++ react/ssr-demo/app.ts
@@ -4,8 +4,11 @@ import { renderToString } from 'react-dom/server'
 const app = express()
 
 app.get('/', (req, res) => {
-  const html = renderToString(<Home />)
+  const html = renderToString(Home())
   res.send(html)
+})
+app.get('/hello', (req, res) => {
+  res.send('<h1>hello</h1>')
 })
 
 app.listen(3000, () => {
--- react/ssr-demo/components/Home.tsx
+++ react/ssr-demo/components/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      Home
+      <button onClick={() => alert('click')}>click</button>
+    </div>
+  )
 }
 
 export default Home
