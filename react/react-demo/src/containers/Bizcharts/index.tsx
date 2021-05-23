bf76d04a9f54e4f1698d778d1df5941d0119bef0
zhourunkai <944268618@qq.com>
2021-05-23 22:02:01 +0800

    feat(bizcharts): add some demos

--- react/react-demo/src/containers/Bizcharts/index.tsx
+++ react/react-demo/src/containers/Bizcharts/index.tsx
@@ -6,6 +6,7 @@ import Histogram from '@/components/Bizcharts/01_学生成绩分布直方图'
 import Column from '@/components/Bizcharts/02_学校成绩分布柱状图'
 import ColumnClass from '@/components/Bizcharts/03_班级成绩报告柱状图'
 import BoxPlot from '@/components/Bizcharts/04_箱型图'
+import BulletChart from '@/components/Bizcharts/05_子弹图'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Column />
       <ColumnClass />
       <BoxPlot />
+      <BulletChart />
     </Temp>
   )
 }
