16fac70403b13298bd53c946557b77233ad4122e
zhourunkai <944268618@qq.com>
2021-05-28 09:10:51 +0800

    feat(trao): music

--- miniprogram/taro-music/src/pages/index/index.tsx
+++ miniprogram/taro-music/src/pages/index/index.tsx
@@ -12,8 +12,6 @@ export default class Index extends Component {
 
   componentDidMount () { }
 
-  componentWillUnmount () { }
-
   componentDidShow () { }
 
   componentDidHide () { }
@@ -21,12 +19,7 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+        hello taro
       </View>
     )
   }
