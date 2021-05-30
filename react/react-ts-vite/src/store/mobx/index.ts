78037e6c0c5c6f5935d7cd1bfd4ff0fe22d9ef2c
zhourunkai <944268618@qq.com>
2021-05-30 23:45:56 +0800

    feat(mobx): init

--- react/react-ts-vite/src/store/mobx/index.ts
+++ react/react-ts-vite/src/store/mobx/index.ts
@@ -1,10 +1,7 @@
 import { action, observable } from 'mobx'
 
 class Store {
-  @observable cache = { queue: [] }
-  @action.bound refresh() {
-    this.cache.queue.push(1)
-  }
+
 }
 
 export default Store
