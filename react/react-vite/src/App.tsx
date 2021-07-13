a2630a498458d385ef8fd60a42b3b121c6fd832c
zhourunkai <944268618@qq.com>
2021-07-13 19:52:01 +0800

    feat(ui): ui

--- react/react-vite/src/App.tsx
+++ react/react-vite/src/App.tsx
@@ -1,83 +1,79 @@
 import React, { FC } from 'react'
-import { Button } from 'antd'
-import { BrowserRouter as Router, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink, useHistory } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import routes from '@router/index'
+
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
+import { FormOutlined } from '@ant-design/icons';
+
+import '@style/antd.css'
+
+const { SubMenu } = Menu;
+const { Content, Sider, Header, Footer } = Layout;
+
+
+
 const App: FC = () => {
+
+  const history = useHistory()
+
   return (
-    <Router>
-      {routes.map((route, idx) => {
-        return (
-          <NavLink to={route.path} key={idx}>
-            <Button>{route.path.slice(1).toUpperCase()}</Button>
-          </NavLink>
-        )
-      })}
-      {renderRoutes(routes)}
-    </Router>
+    <Layout style={{ height: '100vh' }}>
+      <Header className="header">
+        {/* todo */}
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+            >
+              {
+                routes.map((route, idx) => {
+                  const { routes: childRoutes, path } = route
+                  return (
+                    <SubMenu icon={<FormOutlined />} title={route.path.slice(1)} key={idx}>
+                      {
+                        childRoutes.map((route, idx) => {
+                          const { path } = route
+                          const childPath = path.split('/')
+                          const _childPath = childPath[childPath.length - 1]
+                          return (
+                            <Menu.Item key={_childPath} onClick={() => history.push(path)}>{_childPath}</Menu.Item>
+                          )
+                        })
+                      }
+                    </SubMenu>
+                  )
+                })
+              }
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Router>
+              {routes.map((route, idx) => {
+                return (
+                  <NavLink to={route.path} key={idx}>
+                    <Button>{route.path.slice(1).toUpperCase()}</Button>
+                  </NavLink>
+                )
+              })}
+              {renderRoutes(routes)}
+            </Router>
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>©2021 Created by Chou</Footer>
+    </Layout>
   )
 }
 
 export default App
-
-
-// import { Button } from 'antd'
-// import { BrowserRouter as Router, NavLink } from 'react-router-dom'
-// import { renderRoutes } from 'react-router-config'
-// import routes from '@router/index'
-
-// import { Layout, Menu, Breadcrumb } from 'antd';
-// import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-
-// const { SubMenu } = Menu;
-// const { Content, Sider } = Layout;
-
-// ReactDOM.render(
-//   <Layout>
-//     <Layout>
-//       <Sider width={200} className="site-layout-background">
-//         <Menu
-//           mode="inline"
-//           defaultSelectedKeys={['1']}
-//           defaultOpenKeys={[routes[0].path]}
-//           style={{ height: '100%', borderRight: 0 }}
-//         >
-//           {
-//             routes.map((route, idx) => {
-//               const { routes: childRoutes, path } = route
-//               return (
-//                 <SubMenu icon={<UserOutlined />} title={route.path.slice(1)} key={idx}>
-//                   {
-//                     childRoutes.map((route, idx) => {
-//                       return (
-//                         <Menu.Item key={route.path}>{route.path}</Menu.Item>
-//                       )
-//                     })
-//                   }
-//                 </SubMenu>
-//               )
-//             })
-//           }
-//         </Menu>
-//       </Sider>
-//       <Layout style={{ padding: '0 24px 24px' }}>
-//         <Breadcrumb style={{ margin: '16px 0' }}>
-//           <Breadcrumb.Item>Home</Breadcrumb.Item>
-//           <Breadcrumb.Item>List</Breadcrumb.Item>
-//           <Breadcrumb.Item>App</Breadcrumb.Item>
-//         </Breadcrumb>
-//         <Content
-//           className="site-layout-background"
-//           style={{
-//             padding: 24,
-//             margin: 0,
-//             minHeight: 280,
-//           }}
-//         >
-//           Content
-//         </Content>
-//       </Layout>
-//     </Layout>
-//   </Layout>,
-//   document.getElementById('root')
-// );
--- react/react-vite/src/index.tsx
+++ react/react-vite/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'antd/dist/antd.css'
 import '@utils/mock'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
