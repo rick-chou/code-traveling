1f371c43808ed7bd16c301da5d84461543b376df
zhourunkai <nanshu.zhou@tuya.com>
2021-08-09 18:59:43 +0800

    feat(react): redux

--- react/react-vite/src/store/redux/index.ts
+++ react/react-vite/src/store/redux/index.ts
@@ -3,10 +3,12 @@ import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import home from './home/reducers';
 import article from './article/reducers';
+import counter from './counter/reducers';
 
 const rootReducer = combineReducers({
   home,
   article,
+  counter,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
