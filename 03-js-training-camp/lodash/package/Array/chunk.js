3da3568a08eaa6945ba4f6b28d21ff0a8756af49
nanshu <nanshu.zhou@tuya.com>
2022-02-20 01:06:32 +0800

    feat(js): array chunk

---
+++ 03-js-training-camp/lodash/package/Array/chunk.js
@@ -0,0 +1,38 @@
+const { chunk } = require('lodash');
+
+/**
+ * chunk(array, [size=1])
+ * array (Array): 需要处理的数组
+ * [size=1] (number): 每个数组区块的长度
+ */
+
+chunk(['a', 'b', 'c', 'd'], 2);
+// => [['a', 'b'], ['c', 'd']]
+
+chunk(['a', 'b', 'c', 'd'], 3);
+// => [['a', 'b', 'c'], ['d']]
+
+/**
+ *
+ * @param {Array} array
+ * @param {number} size
+ */
+const _chunk = (array, size = 1) => {
+  if (!Array.isArray(array) || !array.length || !size) {
+    return [];
+  }
+  let resIndex = 0,
+    length = Math.ceil(array.length / size);
+  const result = Array(length);
+  while (resIndex < length) {
+    result[resIndex] = array.slice(resIndex * size, (resIndex + 1) * size);
+    resIndex++;
+  }
+  return result;
+};
+
+console.log(_chunk(['a', 'b', 'c', 'd'], 2));
+console.log(_chunk(['a', 'b', 'c', 'd'], 3));
+console.log(_chunk([], 3));
+console.log(_chunk(null, 3));
+console.log(_chunk(4, 3));
