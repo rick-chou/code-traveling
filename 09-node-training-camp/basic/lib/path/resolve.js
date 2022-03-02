1cf223838ae912573875b711f5e22733901aac59
nanshu <944268618@qq.com>
2022-03-02 16:51:45 +0800

    feat(node lib): path

---
+++ 09-node-training-camp/basic/lib/path/resolve.js
@@ -0,0 +1,15 @@
+/**
+ * 方法将路径或路径片段的序列解析为绝对路径
+ */
+
+const path = require('path');
+
+path.resolve('/foo/bar', './baz');
+// 返回: '/foo/bar/baz'
+
+path.resolve('/foo/bar', '/tmp/file/');
+// 返回: '/tmp/file'
+
+path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif');
+// 如果当前工作目录是 /home/myself/node，
+// 则返回 '/home/myself/node/wwwroot/static_files/gif/image.gif'
