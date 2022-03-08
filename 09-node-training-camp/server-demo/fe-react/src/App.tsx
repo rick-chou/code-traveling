00e70db976bc4a9ae0426c092477bae01ea685a5
nanshu <944268618@qq.com>
2022-03-08 23:59:28 +0800

    feat(node): fe home page

--- 09-node-training-camp/server-demo/fe-react/src/App.tsx
+++ 09-node-training-camp/server-demo/fe-react/src/App.tsx
@@ -1,20 +1,7 @@
-import ReactCodeInput from 'react-code-input';
+import Home from './pages/home';
 
 function App() {
-  return (
-    <div>
-      <h1>React && Koa && Mysql... TODO</h1>
-      <ReactCodeInput
-        type="password"
-        fields={6}
-        name={'password'}
-        inputMode={'numeric'}
-        onChange={(v) => {
-          console.log(v);
-        }}
-      />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
