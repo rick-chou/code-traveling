693682d7c48e1762e211e6a48ffe184bdcd3515c
chou <944268618@qq.com>
2022-03-30 14:01:19 +0800

    feat(node-demo-be): api error log

--- 09-node-training-camp/server-demo/be-express/src/utils/index.ts
+++ 09-node-training-camp/server-demo/be-express/src/utils/index.ts
@@ -1,14 +1,20 @@
-const validateParams = async (res: any, statement: Promise<any>) => {
-  return new Promise((resolve, reject) => {
-    statement.then(resolve).catch((err) => {
-      res.status(400);
-      res.json({
-        message: err.message,
-        status: false,
-      });
-      reject(err);
+import { logger } from '../log';
+
+const validateParams = async (req: any, res: any, statement: Promise<any>) => {
+  try {
+    await statement;
+  } catch (error: any) {
+    logger('error').error({
+      api: req.originalUrl,
+      param: req.body,
+      error: error.message,
     });
-  });
+    res.status(400);
+    res.json({
+      message: error.message,
+      status: false,
+    });
+  }
 };
 
 const success = (res: any) =>
