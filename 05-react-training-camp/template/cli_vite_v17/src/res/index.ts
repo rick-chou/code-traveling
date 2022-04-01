8977df331c3a0dfc1e10f733d2ccdb46598c19a4
chou <944268618@qq.com>
2022-04-01 16:20:19 +0800

    feat(react-template): v1
    jest / tailwind / vite / typescript

--- 05-react-training-camp/template/cli_vite_v17/src/res/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as bgImg } from './bg.png';
-export { default as logoImg } from './logo.png';
--- 05-react-training-camp/template/diu_vite_v17/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './app.module.scss';
-
-export default function App() {
-  return (
-    <div className={styles.container}>
-      <h1>Hello Vite</h1>
-    </div>
-  );
-}
