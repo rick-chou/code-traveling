29511de6615c5c408c631d7ea41e5f0aa30efc47
chou <944268618@qq.com>
2022-04-25 16:16:36 +0800

    📦 : @nanshu/cli 迁移

--- 09-node-training-camp/cli/demo/rimraf.js
+++
@@ -1,9 +0,0 @@
-const rm = require('rimraf').sync;
-const fs = require('fs').promises;
-
-fs.mkdir('./test').then(() => {
-  setTimeout(() => {
-    fs.rmdir('./test');
-    // rm('./test');
-  }, 3000);
-});
