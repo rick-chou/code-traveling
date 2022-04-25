5d884e087790d8090a74e2ecbf294a43a110615c
chou <944268618@qq.com>
2022-04-25 14:42:21 +0800

    📝 : mysql basic syntax

---
+++ 15-database-training-camp/mysql/basic/01_create_database.sql
@@ -0,0 +1,11 @@
+-- 删除一个数据库
+drop database test;
+
+-- 新建一个数据库 test 如果不存在的话
+create database if not exists test;
+
+-- 显示所有数据库
+show databases;
+
+-- 使用 test 数据库
+use test;
---
+++ 15-database-training-camp/mysql/basic/04_update.sql
@@ -0,0 +1,2 @@
+-- 更新 user_info 表中 user_nick 为 chou 的 user_nickname 字段 为 nanshu
+update user_info set user_nickname = 'nanshu' where user_nickname = 'chou'; 
